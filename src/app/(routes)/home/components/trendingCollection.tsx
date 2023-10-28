910e4ff969dc545c4ff3b4bf0c6450f2d3ffd020
Oleksandr <oleksandrzelenjak@gmail.com>
2023-10-28 23:13:18 +0300

    fixed bugs

--- src/app/(routes)/home/components/trendingCollection.tsx
+++ src/app/(routes)/home/components/trendingCollection.tsx
@@ -39,10 +39,10 @@ export default function TrendingCollection() {
         <div className='flex gap-[30px] justify-center'>
           {NFT_DATA.map((nft, index) => (
             <div key={index} className='mt-[60px]'>
-              <Image src={nft.imageNft} alt="nft" />
+              <Image src={nft.imageNft} alt="nft" width={330} height={330}/>
               <div className='flex mt-[10px] gap-[15px]'>
-                <Image src={nft.imageNft} alt="nft" className='w-[100px] h-[100px]' />
-                <Image src={nft.imageNft} alt="nft" className='w-[100px] h-[100px]' />
+                <Image src={nft.imageNft} alt="nft" width={100} height={100} />
+                <Image src={nft.imageNft} alt="nft" width={100} height={100} />
                 <div className='flex justify-center items-center bg-[#A259FF] w-[100px] h-[100px] rounded-md'>
                   {nft.likesCount}+
                 </div>
@@ -50,7 +50,7 @@ export default function TrendingCollection() {
               <div className='flex flex-col gap-[10px] mt-[15px]'>
                 <h1>{nft.title}</h1>
                 <div className="flex gap-[12px]">
-                  <Image src={nft.imageAvatar} alt='Avatar' />
+                  <Image src={nft.imageAvatar} alt='Avatar' width={24} height={24}/>
                   <div>{nft.artistName}</div>
                 </div>
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import  HomePage from "./(routes)/home/page";
 
 export default function MainPage() {
 	return (
-		<div>
+		<>
 			<HomePage />
-		</div>
+		</>
 	);
 }

6c24eba43865b1345cba221b90da28b7f7d6f8f3
maevskyy <maevskijdimitrij1@gmail.com>
2023-10-28 18:46:18 +0300

    changed layout

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,17 +1,17 @@
-import Layout from '@/components/PageLayout';
 
 
 export default function HomePage() {
 	return (
-		<Layout title='Market'>
-			{/* <TheHeader />
-			<main className='flex flex-1 h-[100vh]'></main> */}
+		<div className="">HomePage</div>
+// 		<Layout title='Market'>
+// 			{/* <TheHeader />
+// 			<main className='flex flex-1 h-[100vh]'></main> */}
 			
-{/* {Array(25).fill('').map((el) => (
-	<div className="">asdasd</div>
-))} */}
-{/* 
-			<TheFooter /> */}
-		</Layout>
+// {/* {Array(25).fill('').map((el) => (
+// 	<div className="">asdasd</div>
+// ))} */}
+// {/* 
+// 			<TheFooter /> */}
+// 		</Layout>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import  HomePage from "./(routes)/home/page";
-import {Space_Mono} from 'next/font/google'
 
-const spaceMono = Space_Mono({weight: ['400'], subsets: ['latin']}) 
 
 export default function MainPage() {
 	return (
-		<div className={spaceMono.className}>
+		<div>
 			<HomePage />
 		</div>
 	);

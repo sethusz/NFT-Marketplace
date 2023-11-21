00d473c0977d3bbd90bc9f8dbea100c8c15894fa
maevskyy <maevskijdimitrij1@gmail.com>
2023-11-22 01:18:49 +0200

    added images path into next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dmi-nfts-collection.s3.eu-north-1.amazonaws.com',
+                port: '',
+                pathname: '/photos/*'
+            }
+        ]
+    }
+}
 module.exports = nextConfig
